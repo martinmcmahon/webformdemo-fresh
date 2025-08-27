7cc4851510935d4ac2d2b2a62417d0d7aff821ee
Martin McMahon <martin.mcmahon@kerv.com>
2025-08-27 10:07:20 +0100

    Update App.js and fix API configuration for deployment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> <!--Complaints Form - Updated at 09:11 PM BST -->
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
