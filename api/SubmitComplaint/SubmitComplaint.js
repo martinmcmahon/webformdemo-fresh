76cfa830a8e4c0fb60c49253eb2f53340fcb6d83
Martin McMahon <martin.mcmahon@kerv.com>
2025-08-28 21:08:32 +0100

    Update SubmitComplaint.js to use CUSTOM_SERVICE_BUS_CONNECTION

--- api/SubmitComplaint/SubmitComplaint.js
+++ api/SubmitComplaint/SubmitComplaint.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req) {
   context.log('Function triggered for /api/submitComplaint');
 
-  const connectionString = process.env.AzureWebJobsServiceBusConnectionString;
+  const connectionString = process.env.CUSTOM_SERVICE_BUS_CONNECTION;
   context.log(`Service Bus connection string: ${connectionString ? 'Found' : 'Not found'}`);
   if (!connectionString) {
     context.log('Error: Service Bus connection string is missing');
